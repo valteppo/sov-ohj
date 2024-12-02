a52a08b5411263d8f38e59ef6db4d4ebb1de243d
Valtteri Teppo <valtteri.teppo@edu.turkuamk.fi>
2024-12-02 12:12:29 +0200

    Kommentit ja niiden migraatio tehty, lisätty admin hallinnointiin, visible tagi defaulttina päällä

--- nettikirppis/models.py
+++ nettikirppis/models.py
@@ -10,8 +10,19 @@ class Item(models.Model):
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateField(auto_now_add=True)
-    visible = models.BooleanField()
+    visible = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} : {self.price}"
     
+class Comment(models.Model):
+    """
+    Ihmisten myytäviin tavaroihin liittyvät ostajien kommentit, kyselyt, pyynnöt, tarkennukset, tarjoukset, ym.
+    """
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.text
